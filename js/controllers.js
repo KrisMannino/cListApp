69f13419578ce54ec87103ecd4e69ed71c977e4a
KrisMannino <KrisMannino@gmail.com>
2015-01-15 22:47:58 -0600

    added factory

--- js/controllers.js
+++ js/controllers.js
@@ -4,59 +4,56 @@ var ng = angular
 
   ng.module('CListApp', ['ngRoute', 'firebase'])
 
-  .controller("ResultsController", function($scope, $firebase, $interval, $http) {
-    var myDataRef = new Firebase("https://clistapp.firebaseio.com/");
-    var sync = $firebase(myDataRef);
-    var syncObject = sync.$asObject();
+  .controller("ResultsController",[ "$scope", "$firebase", "$http", "searchFactory", function($scope, $firebase, $interval, $http, searchFactory) {
+    var fbDataRef = new Firebase('https://clistapp.firebaseio.com/');
+    var sync = $firebase(fbDataRef);
+    var fbObject = sync.$asObject();
+    var fbSnapshot = {};
 
-    syncObject.$bindTo($scope, "newData");
+      fbObject.$bindTo($scope, "newData");
 
-    myDataRef.on("value", function(snapshot) {
-      console.log(snapshot);
+      console.log(fbObject);
 
-      snapshot.forEach(function(fbSearch) {
-        var key = fbSearch.val();
-        var id = fbSearch.key();
-        console.log(id);
-        var searchWord = key.keyword.term;
-        console.log(searchWord);
-      })
-    });
+      var fredSnapshot;
+      fbDataRef.once('value', function(dataSnapshot) {
+        // store dataSnapshot for use in below examples.
+        fbSnapshot = dataSnapshot;
+        var fbSnapshotdata = fbSnapshot.val();
+        // data will now contain an object like:
+        // { name: { first: 'Fred', last: 'Flintstone' }, rank: 1000 }.
+        console.log(fbSnapshotdata);
 
+        fbSnapshot.forEach(function(fbSearch) {
+          var key = fbSearch.val();
+          var id = fbSearch.key();
+          var searchKey = key.keyword.term;
+          var fbUpdateData;
+          console.log(searchKey);
+          console.log(id);
 
-      $scope.searchAgain= function(searchword){
-        $interval(function(){
-        console.log("searching again");
-        var newData = [];
-        var theWord = searchword;
-        var url = 'http://search.3taps.com?auth_token=11a2ac1d6fd4d8a9dcbd221445790888&retvals=images,heading,location&heading='+theWord+'&rpp=&has_image1'
-        function getJSONP(url, cbName){
-          var $script = document.createElement('script');
-          document.body.appendChild($script);
-        }
-        $http.get(url)
-        .success(function(data){
-          $scope.newData = data;
-          $scope.newData.keyword = $scope.NewSearch;
-/*          myDataRef.push($scope.newData);
-*/          console.log($scope.newData);
+          /*$scope.fbUpdateData = $scope.searchFactory.searchAgain(searchKey);
+          console.log(fbUpdateData);*/
+
+          //sync.$update(fbUpdateData)
         })
-        .error(function(err){
-          console.log(err);
-        });
+      })
+  }
+])
 
-    }, 10000);
-  };
 
-  })
 
-  .controller ('SearchController', function($scope, $http, $firebase){
 
-    var myDataRef = new Firebase('https://clistapp.firebaseio.com/');
+
+
+
+
+  .controller ('SearchController',[ "$scope", "$http", "$firebase", function($scope, $http, $firebase){
+
+   var myDataRef = new Firebase('https://clistapp.firebaseio.com/');
     var sync = $firebase(myDataRef);
     var profileObject = sync.$asObject();
 
-    //profileObject.$bindTo($scope, "data");
+    profileObject.$bindTo($scope, "data");
 
     $scope.searchWord= function(){
       var newData = [];
@@ -79,7 +76,8 @@ var ng = angular
       }
 
 
-})
+}
+])
 
       /*  $scope.searchAgain= function(word){
           var newData = [];
